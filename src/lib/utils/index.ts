14953b72e6f0baeaa3fc9dcf0d357744ff9592a0
Sean Reichel <sean.p.reichel@gmail.com>
2026-06-13 05:51:29 -0400

    new post and some image fuckery

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,6 +1,7 @@
 import { ImageLoader } from "next/image";
 
 export const mdxImageLoader: ImageLoader = ({ src }) => {
+  console.log({ p: process.env.NEXT_PUBLIC_BASE_URL, src })
   const url = new URL(`${process.env.NEXT_PUBLIC_BASE_URL}${src}`);
   return url.toString();
 };
